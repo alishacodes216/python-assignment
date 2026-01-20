99b68f6bd477244ecdc5bc95d7cfcb54f68aa578
alishacodes216 <am6359648@gmail.com>
2026-01-20 20:20:08 +0530

    Python assignment submission - Task1

---
+++ Task1.py
@@ -0,0 +1,7 @@
+a = int(input('enter first number: '))
+b = int(input('enter second number: '))
+
+print('\nAddition:', a+b)
+print('Subtraction :', a-b)
+print('Multiplication :', a*b)
+print('Division :', a/b)
