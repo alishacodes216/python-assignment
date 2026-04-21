ba6a12171dc1b51512336d131a358a3d3bc8b2da
alishacodes216 <am6359648@gmail.com>
2026-04-21 17:58:11 +0530

    Add files via upload

---
+++ Task4.py
@@ -0,0 +1,6 @@
+#----------- QUESTION 2 -----------
+# Sum of integers from 1 to 50 using a loop
+sum =0
+for num in range(1,51):
+    sum = sum + num
+print('sum of all int no is from 1 to 50 is ',sum)
